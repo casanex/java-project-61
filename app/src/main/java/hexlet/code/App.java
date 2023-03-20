073ea171926aef476af0b1b26234a66949bda702
casanex <lonelyishtar@gmail.com>
2023-03-20 21:47:53 +0300

    add some code

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,15 @@
 package hexlet.code;
 
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter \n1 - Greet \n2 - Even \n3 - Calc \n4 - GCD \n5 - Progression \n6 - Prime \n0 - exit");
+        String choise = scanner.next();
+        System.out.println("Your choise: " + choise);
+        Cli.greet();
 
-        System.out.println("Welcome to the Brain Games!");
-        Cli cli = new Cli();
-        cli.greet();
 
     }
 }
