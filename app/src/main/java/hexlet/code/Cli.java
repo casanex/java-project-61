5baf56f6a98aed348c6da49b739a26335225cae1
casanex <lonelyishtar@gmail.com>
2023-03-20 20:37:29 +0300

    renew file cli

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,11 +2,11 @@ package hexlet.code;
 
 import java.util.Scanner;
 public class Cli {
- public static void greet() {
-   Scanner scanner = new Scanner(System.in);
-   System.out.println("May I have your name?");
-   String userName = scanner.next();
-   System.out.println("Hello, " + userName + "!");
-  scanner.close();
- }
+    public static void greet() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("May I have your name?");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        scanner.close();
+    }
 }
