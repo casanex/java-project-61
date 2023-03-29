56b53dea197b751abca008801ff0d987418619ee
casanex <lonelyishtar@gmail.com>
2023-03-29 22:04:19 +0300

    last fix

--- app/src/main/java/hexlet/code/Greet.java
+++ app/src/main/java/hexlet/code/Greet.java
@@ -6,7 +6,7 @@ public class Greet {
     public static void greet() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(" Welcome to the Brain Games!");
-        System.out.println(" May i have your name?");
+        System.out.println(" May I have your name?");
         String userName = scanner.next();
         System.out.println(" Hello, " + userName + "!");
 
