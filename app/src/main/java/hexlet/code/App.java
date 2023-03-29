4326426e86a66a7c4fa2383746274c1240c9187d
casanex <lonelyishtar@gmail.com>
2023-03-29 21:51:12 +0300

    fix bugs2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,16 +10,16 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the game number and press Enter. ");
-        System.out.println("1 - Greet");
-        System.out.println("2 - Even");
-        System.out.println("3 - Calc");
-        System.out.println("4 - GCD");
-        System.out.println("5 - Progression");
-        System.out.println("6 - Prime");
-        System.out.println("0 - exit");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println(" 1 - Greet");
+        System.out.println(" 2 - Even");
+        System.out.println(" 3 - Calc");
+        System.out.println(" 4 - GCD");
+        System.out.println(" 5 - Progression");
+        System.out.println(" 6 - Prime");
+        System.out.println(" 0 - exit");
         String choise = scanner.next();
-        System.out.println("Your choise: " + choise);
+        System.out.println(" Your choise: " + choise);
 
         switch (choise) {
             case "1" -> Greet.greet();
--- app/src/main/java/hexlet/code/Greet.java
+++ app/src/main/java/hexlet/code/Greet.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Greet {
     public static void greet() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("May i have your name?");
+        System.out.println(" Welcome to the Brain Games!");
+        System.out.println(" May i have your name?");
         String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
+        System.out.println(" Hello, " + userName + "!");
 
     }
 }
