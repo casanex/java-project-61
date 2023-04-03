f5bb20369cf5b998e9066dccb4e08fd0b847fdca
casanex <lonelyishtar@gmail.com>
2023-04-03 22:51:55 +0300

    add fix

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -30,7 +30,7 @@ public class Engine {
             System.out.println("Correct!");
 
         }
-        System.out.println("Congratulations, " + userName);
+        System.out.println("Congratulations, " + userName + "!");
 
 
         scanner.close();
